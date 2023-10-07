4d5317c57143e1f6858abee0f9ef535c40136159
Kevin Quesada <kevin.qj@estudiantec.cr>
2023-10-07 13:08:49 -0600

    squid runlist

--- chef-repo/cookbooks/squid/recipes/squid-cache.rb
+++ chef-repo/cookbooks/squid/recipes/squid-cache.rb
@@ -8,15 +8,6 @@ package 'squid' do
   action :install
 end
 
-# Configura Squid
-template '/etc/squid/squid.conf' do
-  source 'squid.conf.erb' # Crea un archivo ERB para configurar Squid
-  owner 'proxy'
-  group 'proxy'
-  mode '0644'
-  notifies :restart, 'service[squid]'
-end
-
 remote_file '/etc/squid/squid.conf' do
 	source 'file:///home/ubuntu/Chef-Repo-PO-Redes/chef-repo/Configuration%20Files/squid.conf'
 	owner 'root'
