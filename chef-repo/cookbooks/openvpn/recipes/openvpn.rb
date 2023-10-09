64e93c8539d20c95fe40ad271979534956d18fb1
Daniel Araya Sambucci <danielarayasambucci@estudiantec.cr>
2023-10-08 19:18:32 -0600

    Automated installation working. Testing alternative solution

--- chef-repo/cookbooks/openvpn/recipes/openvpn.rb
+++ chef-repo/cookbooks/openvpn/recipes/openvpn.rb
@@ -5,15 +5,24 @@
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
 execute 'Ejecutar_Instalacion_OpenVPN' do
-	command 'bash openvpn-install.sh'
-	cwd '/home/ubuntu/Chef-Repo-PO-Redes/chef-repo/Configuration%20Files/'
+	command 'bash /home/ubuntu/Chef-Repo-PO-Redes/chef-repo/Configuration\ Files/openvpn-ubuntu-install.sh'
 	action :run
-	notifies :run, 'remote_file[Copiar_Archivo_Configuracion_Cliente]', :immediately
+	notifies :run , 'execute[Copiar_Archivo_Configuracion_Cliente]', :immediately
 end
 
-execute 'Copiar archivo desde /root' do
-	command 'sudo cp /root/Desktop.ovpn /home/ubuntu/'
-	action :run
+execute 'Copiar_Archivo_Configuracion_Cliente' do
+	command 'bash /home/ubuntu/Chef-Repo-PO-Redes/chef-repo/Configuration\ Files/Desktop.sh'
+	action :nothing
 	not_if { ::File.exist?('/home/ubuntu/Desktop.ovpn') }
-  end
+end
 
+#execute 'Copiar_Archivo_Configuracion_Cliente' do
+#	command 'bash /home/ubuntu/Chef-Repo-PO-Redes/chef-repo/Configuration\ Files/Desktop.sh'
+#	action :run
+#end
+
+
+service 'openvpn' do
+	action :restart
+end
+  
